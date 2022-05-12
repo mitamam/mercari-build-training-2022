c978881470c22eb2dd9dd32f05045a71737b77be
Mitama Masuda <mitama.md@gmail.com>
2022-05-12 14:05:05 +0900

    Fix to store form data to items.json and return response of items

--- go/app/main.go
+++ go/app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,12 +13,23 @@ import (
 	"github.com/labstack/gommon/log"
 )
 
+var items Items
+
 const (
 	ImgDir = "image"
 )
 
 type Response struct {
 	Message string `json:"message"`
+}
+
+type Items struct {
+	Items []Item `json:"items"`
+}
+
+type Item struct {
+	Name     string `json:"name"`
+	Category string `json:"category"`
 }
 
 func root(c echo.Context) error {
@@ -31,9 +43,60 @@ func addItem(c echo.Context) error {
 	category := c.FormValue("category")
 	c.Logger().Infof("Receive item: %s, %s", name, category)
 
+	// Create or Open file with write permission
+	f, err := os.Create("items.json")
+	if err != nil {
+		return err
+	}
+
+	// Store form data to item struct
+	item := Item{
+		Name: name,
+		Category: category,
+	}
+	// Add item struct to slice
+	items.Items = append(items.Items, item)
+
+	// Change items struct to json
+	s, err := json.Marshal(items)
+	if err != nil {
+		return err
+	}
+
+	// Write form data to file
+	_, err = f.Write(s)
+	if err != nil {
+		return err
+	}
+
 	message := fmt.Sprintf("item received: %s, %s", name, category)
 	res := Response{Message: message}
 
+	return c.JSON(http.StatusOK, res)
+}
+
+func getItem(c echo.Context) error {
+	// Open file to read
+	f, err := os.Open("items.json")
+	if err != nil {
+		return err
+	}
+
+	// Read file
+	data := make([]byte, 1024)
+	count, err := f.Read(data)
+	if err != nil {
+		return err
+	}
+
+	// Encode json to struct
+	var i Items
+	err = json.Unmarshal(data[:count], &i)
+	if err != nil {
+		return err
+	}
+
+	res := i
 	return c.JSON(http.StatusOK, res)
 }
 
@@ -71,6 +134,7 @@ func main() {
 
 	// Routes
 	e.GET("/", root)
+	e.GET("/items", getItem)
 	e.POST("/items", addItem)
 	e.GET("/image/:itemImg", getImg)
 
